f302cef76afe6db85e7d61d0db09942069a790df
Quincy Lam <qlam3@my.bcit.ca>
2017-11-01 15:47:38 -0700

    New Changes to have DB to grab words from

--- addWordsToDatabase.php
+++ addWordsToDatabase.php
@@ -6,35 +6,22 @@ mysqli_select_db($db, 'bcit') or
 die(mysqli_error($db));
 
 mysqli_query($db, "
-	CREATE TABLE IF NOT EXISTS list(
+	CREATE TABLE IF NOT EXISTS prefList(
 		word VARCHAR(50) NOT NULL
 	)
 ")or die(mysqli_error($db));
 
 
 
-$contents = file_get_contents("WordList.txt");
+$contents = file_get_contents("Prefix.txt");
 $wordArray = explode("\n", $contents);
 $number = count($wordArray);
 for ($i = 0; $i < $number; $i++) {
         mysqli_query($db, "
-        INSERT INTO list(word) VALUES (
+        INSERT INTO prefList(word) VALUES (
           '$wordArray[$i]'
         )
     ") or die(mysqli_error($db));
 }
-
-$view = mysqli_query($db, "
-	SELECT * FROM list
-");
-
-$rand1 = rand();
-$rand2 = rand();
-
-while($record = mysqli_fetch_assoc($view)) {
-    echo $record['word'];
-}
-
-
 
 ?>
--- index.php
+++ index.php
@@ -1,19 +1,48 @@
 <?php
-$contents = file_get_contents("WordList.txt");
-$wordArray = explode("\n", $contents);
-$contents = file_get_contents("Conjunctions.txt");
-$conjuncArray = explode("\n", $contents);
+$db = mysqli_connect("localhost:3306", "root", "1234")
+or die(mysqli_connect_error());
 
-$rand1 = rand(0, count($wordArray) - 2);
-$rand2 = rand(0, count($wordArray) - 2);
-$rand3 = rand(0, count($conjuncArray) - 2);
+mysqli_select_db($db, 'bcit') or
+die(mysqli_error($db));
+
+$wordRand = rand(1, 235924);
+$wordRand2 = rand(1, 235924);
+$conjRand = rand(1, 9);
+$prefRand = rand(1, 9);
+$prefRand2 = rand(1, 9);
+
+$view1 = mysqli_query($db, "
+	SELECT word FROM wordList WHERE _id = $wordRand;
+");
+
+$view2 = mysqli_query($db, "
+	SELECT word FROM wordList WHERE _id = $wordRand2;
+");
+
+$view3 = mysqli_query($db, "
+	SELECT word FROM conjList WHERE _id = $conjRand;
+");
+
+$view4 = mysqli_query($db, "
+	SELECT word FROM prefList WHERE _id = $prefRand;
+");
+
+$view5 = mysqli_query($db, "
+	SELECT word FROM prefList WHERE _id = $prefRand2;
+");
+
+$randWord1 = mysqli_fetch_assoc($view1);
+$randWord2 = mysqli_fetch_assoc($view2);
+$randConj = mysqli_fetch_assoc($view3);
+$randPref = mysqli_fetch_assoc($view4);
+$randPref2 = mysqli_fetch_assoc($view5);
+
 ?>
 <html xmlns:margin-bottom="http://www.w3.org/1999/xhtml">
 <head>
     <title>Random World Generator</title>
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"
@@ -49,8 +78,12 @@ $rand3 = rand(0, count($conjuncArray) - 2);
 </header>
 <main>
     <div class="center">
-        <h3 class="TestMe" style="display: none;"><?php echo (ucfirst($wordArray[$rand1]) . " " . ucfirst($conjuncArray[$rand3]) . " " . ucfirst($wordArray[$rand2]));?></h3>
-        <button class="btn" onClick="location.reload();">Get new words!</button>
+        <h3 class="TestMe" style="display: none;">
+            <?php echo (ucfirst(trim($randPref['word']) . trim(lcfirst($randWord1['word']))) . " " . ucfirst($randConj['word']) . " " . ucfirst(trim($randPref2['word']) . trim(lcfirst($randWord2['word']))));?>
+            <br/>
+            <br/>
+            <button class="btn" onclick="javascript=window.location.reload()">Reload with new words!</button>
+        </h3>
     </div>
 </main>
 <footer class="page-footer">
@@ -63,7 +96,10 @@ $rand3 = rand(0, count($conjuncArray) - 2);
             <div class="col l4 offset-l2 s12">
                 <h5 class="white-text">Contact:</h5>
                 <div>
-
+                    FakeEmail@FakeEmail.com
+                </div>
+                <div>
+                    604-dont-call-me
                 </div>
             </div>
         </div>
